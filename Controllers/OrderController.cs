04880eacbee87e8470a7f4b2f56c91d4d3adecd2
Samiul Alim <siamahmed64038@gmail.com>
2024-12-05 17:44:31 +0600

    Shipment table

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -1,4 +1,6 @@
 ﻿using CourierManagement.Data;
+using CourierManagement.Models;
+using CourierManagement.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CourierManagement.Controllers
@@ -22,6 +24,42 @@ namespace CourierManagement.Controllers
 
             ViewData["CList"] = customerList;
             return View(parcel);
+        }
+
+        public IActionResult Shipment()
+        {
+            var customerList = _context.Customer
+                .OrderBy(x => x.Customer_ID)
+                .ToList();
+
+            var parcelList = _context.Parcel
+                 .Where(p => p.Status != "Delivered")
+                 .ToList();
+
+            ViewData["CList"] = customerList;
+            return View(parcelList);
+        }
+
+        public IActionResult UpdateStatus(int id)
+        {
+            var parcel = _context.Parcel.Find(id);
+
+            
+            if (parcel == null)
+            {
+                return NotFound();
+            }
+
+            
+            parcel.Status = "Delivered";
+
+            // Save changes to database
+            _context.SaveChanges();
+
+
+            return RedirectToAction("Shipment", "Order");
+ 
+
         }
     }
 }
